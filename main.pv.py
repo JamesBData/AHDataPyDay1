53477746e187037e99fb5435a8798462a0129491
James Barnes <james.barnes11998@gmail.com>
2023-10-04 19:19:22 -0400

    clone

---
+++ main.pv.py
@@ -0,0 +1,46 @@
+first_name = "James"
+print(first_name)
+
+first_var = 3
+second_var = 5
+
+# ordering = input("What would you like?  ")
+# print(ordering)
+
+#y = first_var
+#y = first_name
+# adding variables
+#x = y + 3
+#print(x)
+
+# subtracting variables
+#x = y - 7
+#print(x)
+
+# Multiple variables
+#total = y * 5
+#print(total)
+
+# Modulus
+extra = 13 % 5
+print(extra)
+
+# Divide
+div = 6 / 3
+print(div)
+
+# Floor divide
+sandwiches =  5 // 2
+print(sandwiches)
+
+#exponent
+side = 4
+area = side ** 2
+print(area)
+
+# concatenate
+title = "Sir " + name;
+
+
+
+
