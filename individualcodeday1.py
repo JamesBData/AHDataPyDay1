d4c58896e00487f0d1b6df4829d9ec9146883dfc
James Barnes <james.barnes11998@gmail.com>
2023-10-04 19:51:29 -0400

    Assignment Day 1

---
+++ individualcodeday1.py
@@ -0,0 +1,41 @@
+#Excercise1.1
+print(input("Say echo! "))
+
+#Excercise1.2
+number_input = int(input("Say any number: "))
+print(number_input + 1)
+
+#Excercise1.3
+nfloat_input = float(int(input("Say another number: ")))
+print(nfloat_input + .5)
+
+#Excersize1.4
+num1 = float(input("Say your first number: "))
+num2 = float(input("Say a second number: "))
+print("The total is: " +str(num1 + num2)+".")
+
+#Excersize1.5
+num3 = float(input("Enter a number to multiply: "))
+num4 = float(input("Enter another number: "))
+print("The total is: " +str(num3 * num4)+".")
+
+#Excersize1.6
+num5 = float(input("Enter a number to divide: "))
+num6 = float(input("Enter another number: "))
+print("The total is: " +str(num5 / num6)+".")
+
+#Excersize1.7
+t_f = print(input("The world is flat, true or false: "))
+
+if t_f = "True"
+    print("You entered: True")
+if
+    t_f = "False","false"
+    print("You entered: False")
+
+
+
+
+
+
+
